61d7180f3b7a3d732a599c0a649cdc88b9bcfbf2
Vilaysack Vorachack <55520113+4ss3mbl3rV@users.noreply.github.com>
2026-01-30 15:17:45 +0700

    Fix Docker GPG key installation command

--- linux/kali-docker-install.sh
+++ linux/kali-docker-install.sh
@@ -6,8 +6,8 @@ if [ "$EUID" -ne 0 ]; then
 else
   apt update
   apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
-  curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+  curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   apt update && apt install -y docker-ce docker-ce-cli containerd.io
   systemctl enable docker --now
   usermod -aG docker $USER
