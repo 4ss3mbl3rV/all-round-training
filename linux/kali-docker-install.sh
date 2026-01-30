be481859d4f28ad520bbbca900200a1fd300c6a3
Vilaysack Vorachack <55520113+4ss3mbl3rV@users.noreply.github.com>
2026-01-30 15:21:33 +0700

    Set read permissions on docker.list
    Added read permissions for the Docker source list file.

--- linux/kali-docker-install.sh
+++ linux/kali-docker-install.sh
@@ -8,6 +8,7 @@ else
   apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
   curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  chmod a+r /etc/apt/sources.list.d/docker.list
   apt update && apt install -y docker-ce docker-ce-cli containerd.io
   systemctl enable docker --now
   usermod -aG docker $USER
