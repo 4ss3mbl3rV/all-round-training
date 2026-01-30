e9ea3448a3c79128a252cf1509ce7b63d34fdb7b
Vilaysack Vorachack <55520113+4ss3mbl3rV@users.noreply.github.com>
2026-01-30 12:57:23 +0700

    Add Docker installation script for Debian-based systems
    This script installs Docker on a Debian-based system, checking for root privileges and adding the user to the Docker group.

---
+++ linux/kali-docker-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
+else
+  apt update
+  apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
+  curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+  apt update && apt install -y docker-ce docker-ce-cli containerd.io
+  systemctl enable docker --now
+  usermod -aG docker $USER
+  newgrp docker
+  echo "Finished Docker Installation!"
+fi
